b42391ec67dd7d52da13e28f366558ad5cbc5f79
Xinyi-Wang1 <44707478+Xinyi-Wang1@users.noreply.github.com>
2018-11-03 11:13:49 -0400

    cleaned up, reorganized, and removed a lot of bugs

--- ClassScheduling.py
+++ ClassScheduling.py
@@ -6,32 +6,40 @@ Created on Fri Nov  2 21:17:46 2018
 @author: Elizabeth Chan, Tessa Pham, Xinyi Wang
 
 """
+
 """
-10:48 Fri: an easy way to handle the 0 position is just to have the time id and classes start
-from 0. in reality you're converting the class numbers to numbers from 1 anyways. so why not.
+Structure:
+    
+1. Parsing the file
 
-BECAUSE when you keep an 0 in the front, sorting would be a big issue. including when you
-are using zip. 
+    - parseDemo() function parses the demo data file
+        return students, preferences, classes, times, professorOfClass, classrooms, sizes
+        (right now we do not yet have classrooms and sizes file, to be added later)
+    - parseExcel() function to be added, to parse the excel data
 
+This function incurs cheap costs.
 
-Parsing the Excels part hasn't been tested yet. The current code works for the demo data.
+2. Constructing the data
 
+    - construct(students, preferences, classes)
+        return studentsInClass, overlap, classes
+    
+    After we load the data in by either parseDemo() or parseExcel(), we feed the 
+    data into this construct function. The construct function takes inputs:
+        students, preferences, classes, classrooms, sizesOfClassrooms, times
+    and outputs:
+        studentsInClass, overlap, classes, availableRoomsInTime
+        
+    The complexity of this function is O(k log k)+ O(w), which is the complexity to 
+    process the data
 
-1. Preprocessing the data:
+3. Assign the Classes to times
+    assignClassToTime(c,availableRoomsInTime,professorsInTime,classesInTime,studentsInClass,professorOfClass,times,overlap,classes)
+    
 
-Parse a list of students and a list of their preferences.
-For example:    Name            Preferred Classes
-                Elizabeth       a, b, c, d
-                Tessa           b, e, f, h
-                Xinyi           c, g, m, n
-
-Given a file like above, parse students = {Elizabeth, Tessa, Xinyi} and preferences = {[a, b, c, d], [b, e, f, h], [c, g, m, n]}.
-Arrays we need from parsing: students, preferences, classes
-    students -  array of all students
-    preferences - array of preference lists of all students (parsed in same order as students)
-    classes - array of all classes, each represented by an integer from 1 to m
-    times - array of all time slots, each represented by an integer from 1 to w
 """
+
+
 
 # parsing excel
 import os
@@ -42,7 +50,22 @@ import xlrd
 # write multiple parse functions (for the demo file, for the preference lists of students, etc.) if necessary
 
 # parsing for demo data
-def parse():
+def parseDemo():
+    '''
+Given a file:
+                Name            Preferred Classes
+                Elizabeth       a, b, c, d
+                Tessa           b, e, f, h
+                Xinyi           c, g, m, n
+
+return students = {Elizabeth, Tessa, Xinyi} 
+preferences = {[a, b, c, d], [b, e, f, h], [c, g, m, n]}.
+Arrays we need from parsing: students, preferences, classes
+    students -  array of all students
+    preferences - array of preference lists of all students (parsed in same order as students)
+    classes - array of all classes, each represented by an integer from 1 to m
+    times - array of all time slots, each represented by an integer from 1 to w
+    '''
     students = []
     preferences = [] 
     classes = []
@@ -80,7 +103,6 @@ def parse():
             individualPref = []
 
     DSP1.close()
-
     
     DC = open("demo_constraints.txt", "r") # opens file with name of "test.txt"
     splitDemoCon = DC.read().replace("\t", " ").replace("\n", " ").split(" ")
@@ -94,14 +116,14 @@ def parse():
                 # classes.append(str(j))
                 classes.append(j) # classes[0..13] will store 1 - 14, same as students above!
     
+    
+
     # times
     for i in range(0, int(splitDemoCon[2])):
         times.append(str(i))
- #   print("times: ")
- #   print(times)
 
     # professorOfClass
-    professorOfClass[0] = 0 # class 0 is not valid
+    professorOfClass = [0] # class 0 is not valid
     for i in range(0, len(splitDemoCon)):
         if splitDemoCon[i] == "Teachers":
             for j in range(i + 3, len(splitDemoCon), 2):
@@ -112,30 +134,9 @@ def parse():
  #   print(professorOfClass)
 
     DC.close()
+    ptemp=[[int(u) for u in x] for x in preferences]
+    preferences=ptemp
     
-    # integerFormat = [int(c) for c in classes]
-    # classes = integerFormat
-    # Tessa: We don't need the 2 lines above because classes are already added to the array as integers. See line 97.
-
-# Xinyi: Lizzy makes the start of all arrays a placeholder. I am un-doing it.
-# Tessa: No. What Lizzy did was actually storing classes 1 - 14 as 0 - 13. I changed it to: classes[0..13] stores students 1 - 14, i.e. classes[0] = 1.
-    
-    
-    #>>>????????????????????????
-    
-    # preferences.pop(0) 
-    # classes.pop(0)
-    # students.pop(0)
-    # times.pop(0)
-    # professorOfClass.pop(0) # did you do that??
-    
-    # classes[:]=[x-1 for x in classes]
-    # students[:]=[x-1 for x in students]
-    # times[:]=[x-1 for x in times]
-    # professorOfClass[:]=[x-1 for x in professorOfClass]
-    
-    # Tessa: We don't need this! Just fixed the code above.
-
     return students, preferences, classes, times, professorOfClass
 
 """
@@ -155,18 +156,23 @@ def BMCparse(fileName):
 
 """
 # Another level for constructing the inputs.
-# def construct(students, preferences, classes, times, studentsInClass, overlap):
-def construct(students, preferences, classes, studentsInClass, overlap): # we don't need times for this function
-    # Tessa: Xinyi, please establish: studentsInClass.get(0) = 0; overlap[0][x] = overlap[x][0] = 0.
-    # For overlap, we can initialize a 2D array of all zeroes (0 - 14 each side).
-    
+
+def construct(students, preferences, classes, classrooms, sizesOfClassrooms, times):
+    # studentsInClass: a dictionary (key = class, value = list of students in that class)
+    studentsInClass = {c: [] for c in range(0, 15)}
+    studentsInClass.get(0).append(0)
+    # overlap: a 2D matrix (row = all classes, column = all classes, entry at (i, j) = # of students taking both classes i and j)
+    overlap = [[0 for c in range(0, 15)] for c in range(0, 15)]
     for s, p in zip(students, preferences):
         # for each class c in the preference list of student s
         for c in p:
             # add s to student list of class c
-            studentsInClass.get(c).append(s)
+            if studentsInClass[c]==None:
+                studentsInClass[c]=[s]
+            else:
+                studentsInClass[c].append(s)
             # increment the overlaps of class c with each class in the rest of list p
-            for other_c in p[(p.index(c) + 1)]:
+            for other_c in p[(p.index(c) + 1):]:
                 # in overlap and any other arrays we construct, all classes are 1 less than their original numbers in the data
                 overlap[c][other_c] += 1
                 overlap[other_c][c] += 1
@@ -174,30 +180,36 @@ def construct(students, preferences, classes, studentsInClass, overlap): # we do
     sizes = [len(studentsInClass.get(c)) for c in classes]
     sortedClasses = [x for _, x in sorted(zip(sizes, classes))]
     classes = sortedClasses
-    return studentsInClass, overlap
 
+#sort the classroom from small to big, and pair with their size.
+    sortedClassroom=[(y, x) for x, y in sorted(zip(sizesOfClassrooms, classrooms))]
+    # availableRoomsInTime: a dictionary (key = time, value = list of tuples (room, size), ranked from smallest to largest)
+    availableRoomsInTime = {t: sortedClassroom for t in times}  
+    return studentsInClass, overlap, classes, availableRoomsInTime
 
 
 def assignClassToTime(c,availableRoomsInTime,professorsInTime,classesInTime,studentsInClass,professorOfClass,times,overlap,classes):
     min_overlap = float("inf")
     chosen_time = times[0]
+    
     prof = professorOfClass[classes.index(c)]
 
     for t in times:
         # skip if the professor teaching class c is already teaching another class in this time
-        if len(professorsInTime.get(t)) != 0 & prof in professorsInTime.get(t):
+        if (len(professorsInTime[t]) != 0) & (prof in professorsInTime[t]):
             continue
 
         # skip if no more available rooms
-        if len(availableRoomsInTime.get(t)) == 0:
+        if len(availableRoomsInTime[t]) == 0:
             continue
 
         # skip if number of students in class c is greater than the size of the biggest available room in time t
-        if len(studentsInClass.get(c)) > availableRoomsInTime.get(t)[0]:
+        if len(studentsInClass.get(c)) > availableRoomsInTime[t][-1][1]:
             continue
         
         count = 0
-        for assigned_c in classesInTime.get(t):
+        
+        for assigned_c in classesInTime[t]:
             count += overlap[c][assigned_c]
         
         if count < min_overlap:
@@ -205,15 +217,37 @@ def assignClassToTime(c,availableRoomsInTime,professorsInTime,classesInTime,stud
             chosen_time = t
     
     # add class c to the chosen time
-    classesInTime.get(chosen_time).append(c)
+    classesInTime[chosen_time].append(c)
     # add the professor teaching class c to the list of professors occupied in the chosen time
-    professorsInTime.get(chosen_time).append(prof)
-    # remove the first room from list of available rooms (because they're ranked by size, as well as the classes)
-    availableRoomsInTime.pop(0)
+    professorsInTime[chosen_time].append(prof)
+    # remove the last room from list of available rooms (because they're ranked by size)
+    availableRoomsInTime[chosen_time].pop()
 
 def main():
-    students, preferences, classes, times, professorOfClass = parse()
-    # studentsInClass: a dictionary (key = class, value = list of students in that class)
+    students, preferences, classes, times, professorOfClass = parseDemo()
+    
+    #these two inputs are to be returned by parse() too. 
+    classrooms=[x for x in range(20)]
+    sizesOfClassrooms=[100]*20
+    
+    studentsInClass, overlap, classes, availableRoomsInTime = construct(students, preferences, classes,classrooms, sizesOfClassrooms,times)
+    
+    #Now, ready to assign classes to timeslots. initialize two arrays to store the result first.
+    # classesInTime: a dictionary (key = time, value = list of classes in that time)
+    classesInTime = {t: [] for t in times}
+    # professorsInTime: a dictionary (key = time, value = list of professors teaching a class in that time)
+    professorsInTime = {t: [] for t in times}
+    for c in classes:
+        assignClassToTime(c,availableRoomsInTime,professorsInTime,classesInTime,studentsInClass,professorOfClass,times,overlap,classes)
+    
+    print(classesInTime)
+    return classesInTime
+   
+if __name__ == "__main__":
+    main()
+    
+"""
+for testing, put in main:
     print(students)
     print('\n')
     print(preferences)
@@ -223,26 +257,23 @@ def main():
     print(times)
     print('\n')
     print(professorOfClass)
-"""    studentsInClass = {c: [] for c in classes}
-    # classesInTime: a dictionary (key = time, value = list of classes in that time)
-    classesInTime = {t: [] for t in times}
-    # professorsInTime: a dictionary (key = time, value = list of professors teaching a class in that time)
-    professorsInTime = {t: [] for t in times}
-    # availableRoomsInTime: a dictionary (key = time, value = list of tuples (room, size), ranked from largest to smallest)
-    availableRoomsInTime = {t: [] for t in times}
-# overlap: a 2D matrix (row = all classes, column = all classes, entry at (i, j) = # of students taking both classes i and j)
-    overlap = [[0 for c in classes] for c in classes]
-    studentsInClass, overlap = construct(students, preferences, classes, times, studentsInClass, overlap)
-    parse()
+    print(availableRoomsInTime)  
 
-    for c in classes:
-        assignClassToTime(c,availableRoomsInTime,professorsInTime,classesInTime,studentsInClass,professorOfClass,times,overlap,classes)
+"""
+"""
+To handle the starting element:
+    
+10:48 Fri: an easy way to handle the 0 position is just to have the time id and classes start
+from 0. in reality you're converting the class numbers to numbers from 1 anyways. so why not.
 
-    return classesInTime
-"""    
-if __name__ == "__main__":
-    main()
+BECAUSE when you keep an 0 in the front, sorting would be a big issue. including when you
+are using zip. 
 
+
+Parsing the Excels part hasn't been tested yet. The current code works for the demo data.
+
+
+"""
 """
 Lizzy/Tessa's comments in main
     # call parse methods here if necessary?
@@ -254,10 +285,11 @@ Lizzy/Tessa's comments in main
     # cwd = os.getcwd()
     # os.chdir("brynmawr")
     # fileName = 'bmc-÷÷÷÷÷÷÷÷÷≥data-f17.xls'
-    # fileName = 'bmc-data-f17.xls'
     # BMCdata = pd.read_excel(fileName)
 
     # BMCparse(BMCdata)
 
-    # --- parsing for BMC data  
+    # --- parsing for BMC data
+    
+    
 """
